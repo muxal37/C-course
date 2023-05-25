7e553a0bacf5c4e5e074a69d46fa29af564869c9
MuxaL <Pogger123@yandex.kz>
2023-05-26 01:41:20 +0800

    Remade E14.c

--- E tasks/E14.c
+++ E tasks/E14.c
@@ -8,28 +8,43 @@ int main() {
         scanf("%d", &array[i]);
     }
     
-
-
-    int count[1000] = {0};
+    int ind = 0;
+    int modif[SIZE];
     for (int i = 0; i < SIZE; i++)
     {
-        count[array[i]] += 1;
-    }
-
-    int outp[SIZE], c = 0;
-    for (int i = 0; i < 1000; i++)
-    {
-        if (count[i] >= 2) {
-            outp[c] = i;
-            c++;
-        }
+        modif[i] = SIZE + 1;
     }
     
 
-    for (int i = 0; i < c; i++)
+    for (int i = 0; i < SIZE; i++)
     {
-        printf("%d ", outp[i]);
+        for (int j = i + 1; j < SIZE; j++)
+        {
+            if (array[i] == array[j]) {
+                int flag = 1;
+                for (int k = 0; k < ind; k++)
+                {
+                    if (array[modif[k]] == array[i]){
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag == 1){
+                    modif[ind++] = i;
+                }
+            }
+        }
     }
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (modif[i] == SIZE + 1){
+            break;
+        }
+        printf("%d ", array[modif[i]]);
+    }
+    
+    
+    
     
     return 0;
 }
